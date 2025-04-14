e01a2a2ae6cad47651832dffd5e521d38adb8f8f
Giovanna <giovanna.vasques1101@hotmail.com>
2025-04-14 13:23:55 -0300

    add: classes para consumo de API e desserializacao do JSON para os objetos DadosSerie e DadosEpisodio

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,33 @@
+package br.com.alura.screenmatch;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import br.com.alura.screenmatch.model.DadosEpisodio;
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.service.ConsumoAPI;
+import br.com.alura.screenmatch.service.ConverteDados;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoAPI = new ConsumoAPI();
+		var json = consumoAPI.obterDados("http://omdbapi.com/?t=gilmore+girls&apikey=4def3586");
+		
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+
+		json = consumoAPI.obterDados("http://omdbapi.com/?t=gilmore+girls&Season=1&Episode=1&apikey=4def3586");
+		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
+	}
+
+}
